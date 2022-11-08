8fc840951549adc927aa82bdbf5134b8bd5e8f04
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-09 02:44:37 +0600

    blog section add

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      <h2>Blog page</h2>
+    <div className='px-10 md:px-10 lg:w-4/5 mx-auto my-10 space-y-5'>
+      <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          Difference between SQL and NoSQL
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+
+      <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          What is JWT, and how does it work?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+
+      <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          What is the difference between javascript and NodeJS?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+
+      <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          How does NodeJS handle multiple requests at the same time?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
     </div>
   );
 };
