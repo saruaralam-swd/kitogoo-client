e656d83dae43d8145175636c2a4bef08a2be3dbe
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-09 12:06:51 +0600

    server deploy

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Services from '../LimitServices/LimitServices';
 import Banner from '../Banner/Banner';
 import Facilities from '../Facilities/Facilities';
+import useTitle from '../../../hooks/useTitle';
 
 const Home = () => {
+  useTitle('Home')
   return (
     <div className='bg-[#FCF7F4]'>
       <Banner></Banner>
